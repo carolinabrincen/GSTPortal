cd4f8a9ca50577cbb8a4a2b8322aeaa7a9492d49
ArguellesMA <marco_arguelles97@hotmail.com>
2023-04-11 10:42:40 -0500

    actualizacion de sumatoria y estilo a indicadores

--- src/app/pages/indicadores/indicadores.component.ts
+++ src/app/pages/indicadores/indicadores.component.ts
@@ -34,7 +34,23 @@ export class IndicadoresComponent implements OnInit {
   //=================GETS===========================
   getScoreCard(){
     this.indicadorService.getScoreCard().subscribe(data => {
+
       this.ingresos = data.data.scIng;
+      /*const orderdata: ScoreCard[] = data.data.scIng;
+      let neworderdata = [];
+      neworderdata.push(orderdata[0],orderdata[1],
+      orderdata[2],orderdata[3],orderdata[4],orderdata[5],
+      orderdata[6],orderdata[7],orderdata[8],orderdata[9]
+      ,orderdata[10],orderdata[11],orderdata[12],orderdata[13]
+      ,orderdata[14],orderdata[15],orderdata[16],orderdata[17],
+      orderdata[18],orderdata[19],);
+
+      this.ingresos = neworderdata;
+      console.log(this.ingresos)*/
+
+
+
+      //this.products.sort((a, b) => (a.id < b.id ? -1 : 1));
       this.kilomentros = data.data.scKms;
       this.ingresosKilometros = data.data.scIngXKm;
       this.viajes = data.data.scViajes;
@@ -46,39 +62,119 @@ export class IndicadoresComponent implements OnInit {
 
   }
   onCellPreparedI(e: any) {
-    if (e.rowType == 'groupFooter'){
+    if (e.rowType == 'group'){
 
-      e.cellElement.style.fontSize = '15px';
+      e.cellElement.style.fontSize = '12px';
       e.cellElement.style.background = "#DCDCDC";
-  }
-  }
+    }
 
-
-  onRowPreparedK(event){
-
-  }
-
-  onRowPreparedIK(event){
-
-  }
-
-  onRowPreparedV(event){
-
+    if (e.rowType == 'totalFooter') {
+      e.totalItem.cells.forEach((c: any) => {
+        if (c.cellElement) {
+            c.cellElement.style.fontWeight = "bolder";
+            c.cellElement.style.fontSize = "16px";
+            c.cellElement.style.background = "#ff9460";
+            c.cellElement.style.color = "black"; 
+        }   
+      });
+    }
   }
 
-  onRowPreparedKV(event){
 
+  onRowPreparedK(e){
+ 
+  }
+  onCellPreparedK(e){
+    if (e.rowType == 'group'){
+
+      e.cellElement.style.fontSize = '12px';
+      e.cellElement.style.background = "#DCDCDC";
+    }
+
+    if (e.rowType == 'totalFooter') {
+      e.totalItem.cells.forEach((c: any) => {
+        if (c.cellElement) {
+            c.cellElement.style.fontWeight = "bolder";
+            c.cellElement.style.fontSize = "16px";
+            c.cellElement.style.background = "#ff9460";
+            c.cellElement.style.color = "black"; 
+        }   
+      });
+    }
+  }
+
+  onRowPreparedIK(e){
+    
+  }
+  onCellPreparedIK(e){
+    if (e.rowType == 'group'){
+
+      e.cellElement.style.fontSize = '12px';
+      e.cellElement.style.background = "#DCDCDC";
+    }
+
+    if (e.rowType == 'totalFooter') {
+      e.totalItem.cells.forEach((c: any) => {
+        if (c.cellElement) {
+            c.cellElement.style.fontWeight = "bolder";
+            c.cellElement.style.fontSize = "16px";
+            c.cellElement.style.background = "#ff9460";
+            c.cellElement.style.color = "black"; 
+        }   
+      });
+    }
+  }
+
+  onRowPreparedV(e){
+    
+  }
+  onCellPreparedV(e){
+    if (e.rowType == 'group'){
+
+      e.cellElement.style.fontSize = '12px';
+      e.cellElement.style.background = "#DCDCDC";
+    }
+
+    if (e.rowType == 'totalFooter') {
+      e.totalItem.cells.forEach((c: any) => {
+        if (c.cellElement) {
+            c.cellElement.style.fontWeight = "bolder";
+            c.cellElement.style.fontSize = "16px";
+            c.cellElement.style.background = "#ff9460";
+            c.cellElement.style.color = "black"; 
+        }   
+      });
+    }
+  }
+
+  onRowPreparedKV(e){
+    
+  }
+  onCellPreparedKV(e){
+    if (e.rowType == 'group'){
+
+      e.cellElement.style.fontSize = '12px';
+      e.cellElement.style.background = "#DCDCDC";
+    }
+
+    if (e.rowType == 'totalFooter') {
+      e.totalItem.cells.forEach((c: any) => {
+        if (c.cellElement) {
+            c.cellElement.style.fontWeight = "bolder";
+            c.cellElement.style.fontSize = "16px";
+            c.cellElement.style.background = "#ff9460";
+            c.cellElement.style.color = "black"; 
+        }   
+      });
+    }
   }
 
   separator(value) {
     var str = value.toString().split(".");
     str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return str.join(".");
-}
+  }
 
-scroll(event){
-  console.log(event)
-}
 }
 
 
