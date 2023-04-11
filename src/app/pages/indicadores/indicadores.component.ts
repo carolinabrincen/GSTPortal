a7253f2f30c318c604bfa66233ddb8b0e62159c5
ArguellesMA <marco_arguelles97@hotmail.com>
2023-04-11 14:40:16 -0500

    nuevo grid precio meta indicadores

--- src/app/pages/indicadores/indicadores.component.ts
+++ src/app/pages/indicadores/indicadores.component.ts
@@ -12,10 +12,11 @@ import { ScoreCard } from '../../shared/models/indicadores/scoreCard.model';
 export class IndicadoresComponent implements OnInit {
 
   ingresos: ScoreCard[] = [];
-  kilomentros: ScoreCard[] = []
+  kilomentros: ScoreCard[] = [];
   ingresosKilometros: ScoreCard[] = [];
   viajes: ScoreCard[] = [];
-  kilometroViajes: ScoreCard[] = []
+  kilometroViajes: ScoreCard[] = [];
+  precioMeta: ScoreCard[] = [];
 
   constructor(
     private indicadorService: IndicadoresService
@@ -32,29 +33,38 @@ export class IndicadoresComponent implements OnInit {
   ngAfterViewInit(): void {}
 
   //=================GETS===========================
+  cuautitlan: number;
+  guadalajara: number;
+  hermosillo: number;
+  mexicali: number;
+  orizaba: number;
+  ramisArispe: number;
+  total: number;
+  tultitlan: number;
   getScoreCard(){
     this.indicadorService.getScoreCard().subscribe(data => {
 
       this.ingresos = data.data.scIng;
-      /*const orderdata: ScoreCard[] = data.data.scIng;
-      let neworderdata = [];
-      neworderdata.push(orderdata[0],orderdata[1],
-      orderdata[2],orderdata[3],orderdata[4],orderdata[5],
-      orderdata[6],orderdata[7],orderdata[8],orderdata[9]
-      ,orderdata[10],orderdata[11],orderdata[12],orderdata[13]
-      ,orderdata[14],orderdata[15],orderdata[16],orderdata[17],
-      orderdata[18],orderdata[19],);
-
-      this.ingresos = neworderdata;
-      console.log(this.ingresos)*/
-
-
-
-      //this.products.sort((a, b) => (a.id < b.id ? -1 : 1));
       this.kilomentros = data.data.scKms;
       this.ingresosKilometros = data.data.scIngXKm;
       this.viajes = data.data.scViajes;
       this.kilometroViajes = data.data.scKmsViaje;
+      this.precioMeta = data.data.scPrecioMeta;
+
+      var myPrecioM = data.data.scPrecioMeta;
+      for (var i = 0; i<myPrecioM.length; i++){
+        console.log(myPrecioM[i])
+        this.cuautitlan = myPrecioM[i].cuatitlan;
+        this.tultitlan = myPrecioM[i].tultitlan;
+        this.guadalajara = myPrecioM[i].guadalajara;
+        this.hermosillo = myPrecioM[i].hermosillo;
+        this.mexicali = myPrecioM[i].mexicali;
+        this.orizaba = myPrecioM[i].orizaba;
+        this.ramisArispe = myPrecioM[i].ramosArizpe;
+        this.total = myPrecioM[i].total;
+      }
+
+
     })
   }
 //==============================INGRESOS=========================================
@@ -142,14 +152,76 @@ export class IndicadoresComponent implements OnInit {
   }
 
 //==============================INGRESOS KILOMETROS===============================
+onCellPreparedPM(e){
+  if (e.rowType == 'group'){
+
+    e.cellElement.style.fontSize = '12px';
+    e.cellElement.style.background = "#DCDCDC";
+  }
+}  
+
   onRowPreparedIK(e){
-    
+    if (e.rowType == 'data') {
+      e.cells.forEach((c: any) => {
+
+        if (c.cellElement && c.columnIndex == 2) {
+          if (this.cuautitlan >= c.value) {
+            c.cellElement.style.color = "red";
+          }
+        }
+        if (c.cellElement && c.columnIndex == 3) {
+          if (this.tultitlan >= c.value) {
+            c.cellElement.style.color = "red";
+          }
+        }
+        if (c.cellElement && c.columnIndex == 4) {
+          if (this.guadalajara >= c.value) {
+            c.cellElement.style.color = "red";
+          }
+        }
+        if (c.cellElement && c.columnIndex == 5) {
+          if (this.hermosillo >= c.value) {
+            c.cellElement.style.color = "red";
+          }
+        }
+        if (c.cellElement && c.columnIndex == 6) {
+          if (this.mexicali >= c.value) {
+            c.cellElement.style.color = "red";
+          }
+        }
+        if (c.cellElement && c.columnIndex == 7) {
+          if (this.orizaba >= c.value) {
+            c.cellElement.style.color = "red";
+          }
+        }
+        if (c.cellElement && c.columnIndex == 8) {
+          if (this.ramisArispe >= c.value) {
+            c.cellElement.style.color = "red";
+          }
+        }
+        if (c.cellElement && c.columnIndex == 9) {
+          if (this.total >= c.value) {
+            c.cellElement.style.color = "red";
+          }
+        }
+      });
+    }
+
+    if (e.rowType == 'group'){
+      if (e.cellElement && e.columnIndex == 2) {
+        if (this.cuautitlan >= e.value) {
+          e.cellElement.style.color = "red";
+        }
+      }
+    }
+
   }
   onCellPreparedIK(e){
     if (e.rowType == 'group'){
 
       e.cellElement.style.fontSize = '12px';
       e.cellElement.style.background = "#DCDCDC";
+      
     }
 
     if (e.rowType == 'totalFooter') {
