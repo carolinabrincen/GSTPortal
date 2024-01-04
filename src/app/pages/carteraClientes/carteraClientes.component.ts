d6d62df67893c91945f6ede0816b76888e79da3f
ArguellesMA <marco_arguelles97@hotmail.com>
2024-01-04 10:32:42 -0600

    periodo 202401 cartera

--- src/app/pages/carteraClientes/carteraClientes.component.ts
+++ src/app/pages/carteraClientes/carteraClientes.component.ts
@@ -90,6 +90,7 @@ export class CarteraClientesComponent implements OnInit {
     { id: 10, periodo: 202310 },
     { id: 11, periodo: 202311 },
     { id: 12, periodo: 202312 },
+    { id: 1, periodo: 202401 },
   ];
 
   periodoActual: number;
