8520d0bbcba358955d40b909a06ef9678b0e19eb
ArguellesMA <marco_arguelles97@hotmail.com>
2024-12-17 09:39:41 -0600

    actualizacion disponibilidad mensual

--- src/app/pages/disponibilidadMensual/disponiblidadMensual.component.ts
+++ src/app/pages/disponibilidadMensual/disponiblidadMensual.component.ts
@@ -54,18 +54,18 @@ export class disponibilidadMensualComponent implements OnInit {
   isVisible = false;
 
   meses: any[] = [
-    { id: 1, mes:  1},
-    { id: 2, mes:  2},
-    { id: 3, mes:  3},
-    { id: 4, mes:  4},
-    { id: 5, mes:  5},
-    { id: 6, mes:  6},
-    { id: 7, mes:  7},
-    { id: 8, mes:  8},
-    { id: 9, mes:  9},
-    { id: 10, mes:  10},
-    { id: 11, mes:  11},
-    { id: 12, mes:  12},
+    { id: 1, mes: 'ENERO'},
+    { id: 2, mes: 'FEBRERO'},
+    { id: 3, mes: 'MARZO'},
+    { id: 4, mes: 'ABRIL'},
+    { id: 5, mes: 'MAYO'},
+    { id: 6, mes: 'JUNIO'},
+    { id: 7, mes: 'JULIO'},
+    { id: 8, mes: 'AGOSTO'},
+    { id: 9, mes: 'SEPTIEMBRE'},
+    { id: 10, mes: 'OCTUBRE'},
+    { id: 11, mes: 'NOVIEMBRE'},
+    { id: 12, mes: 'DICIEMBRE'},
   ];
 
   anio: any[] = [
@@ -179,6 +179,7 @@ export class disponibilidadMensualComponent implements OnInit {
   /*======================SELECTE FUNCIONS================================================*/
   selectUdn(value: any) {
     this.selectedMes = value.value;
+    console.log(this.selectedMes)
   }
   selectAnio(value: any) {
     this.selectedAnio = value.value;
